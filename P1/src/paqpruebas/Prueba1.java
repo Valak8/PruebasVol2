915ce72471b4a86a6f8b67b412ea67b7f56557c7
Galder <Galder@DESKTOP-EU1JRUL>
2020-04-16 18:44:25 +0200

    El segundo usuario realiza un cambio!!

--- P1/src/paqpruebas/Prueba1.java
+++ P1/src/paqpruebas/Prueba1.java
@@ -6,6 +6,7 @@ public class Prueba1 {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo 1!!!");
 		System.out.println("El primer usuario realiza un cambio!!");
+		System.out.println("El segundo usuario realiza un cambio!!");
 
 	}
 
