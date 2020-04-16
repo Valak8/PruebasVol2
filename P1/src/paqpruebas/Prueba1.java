190fded38e70c8b2b3bfc9bd938a513ca4faef73
Galder <Galder@DESKTOP-EU1JRUL>
2020-04-16 18:41:33 +0200

    sync useer2

--- P1/src/paqpruebas/Prueba1.java
+++ P1/src/paqpruebas/Prueba1.java
@@ -5,6 +5,7 @@ public class Prueba1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo 1!!!");
+		System.out.println("El primer usuario realiza un cambio!!");
 
 	}
 
