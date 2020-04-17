c28175f4a2ad81102e59e72c9a6c25a5b5ed6419
Galder <Galder@DESKTOP-EU1JRUL>
2020-04-17 19:35:44 +0200

    user 1

--- P1/src/paqpruebas/Prueba1.java
+++ P1/src/paqpruebas/Prueba1.java
@@ -4,23 +4,8 @@ public class Prueba1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Que dia mas maravilloso");
-		System.out.println("No esto es un puto lio");
-		System.out.println("A la vez user 2");
-		System.out.println("Esto vamos a la vez, ok");
-		System.out.println("Hola soy el usuario 2");
-		System.out.println("Hola user 2");
+		System.out.println("dbucdhb");
 		
-		
-		System.out.println("No se que coño hago haciendo esta basura");	
-		System.out.println("Esto es lo peor macho");
-		
-		System.out.println("Buenas noches");
-		System.out.println("Buenos dias");
-		
-		System.out.println("yeyeyeyyeye");
-		System.out.println("wowowoowow");
-		System.out.println("owo");
 	}
 
 }
