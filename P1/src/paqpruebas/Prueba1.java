1eb08098b801c8eefabc221b5fb2283ace126988
Galder <Galder@DESKTOP-EU1JRUL>
2020-04-17 17:29:47 +0200

    t

--- P1/src/paqpruebas/Prueba1.java
+++ P1/src/paqpruebas/Prueba1.java
@@ -4,18 +4,7 @@ public class Prueba1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola Mundo 1!!!");
-		System.out.println("El primer usuario realiza un cambio!!");
-		System.out.println("El segundo usuario realiza un cambio!!");
-		System.out.println("Hola user1");
-		System.out.println("hjsdabhjsd");
-		System.out.println("djsdiujdsiisd");
-		System.out.println("Un nuevo dato");
-		System.out.println("Hola");
-		System.out.println("adios1111111");
-		System.out.println("Hola11111111");
-		System.out.println("Hola user 1");
-		System.out.println("Hola User 2");
+		System.out.println("Hola user 2");
 	}
 
 }
