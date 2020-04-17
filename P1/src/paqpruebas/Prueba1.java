f835a1aebdf473fce6e252a75ec29af04e76cba1
Galder <Galder@DESKTOP-EU1JRUL>
2020-04-17 18:12:27 +0200

    dia maravilloso

--- P1/src/paqpruebas/Prueba1.java
+++ P1/src/paqpruebas/Prueba1.java
@@ -4,17 +4,7 @@ public class Prueba1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola user 2");
-		System.out.println("Hola de  nuevo u2");
-		System.out.println("Hola soy Alice");
-		System.out.println("Hola Alice, que tal?");
-		System.out.println("Bien y tu?");
-		System.out.println("Bueno me tengo que ir");
-		System.out.println("Yo genial, que tal el dia?");
-		System.out.println("Hace sol");
-		System.out.println("No hace viento");
-		System.out.println("Y que lo diagas");
-		System.out.println("este dia es una mierda");
+		System.out.println("Que dia mas maravilloso");
 	}
 
 }
