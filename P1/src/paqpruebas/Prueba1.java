eb9e08da0d5a959ef203dd6ebd79192e302bfc7c
Galder <Galder@DESKTOP-EU1JRUL>
2020-04-17 16:50:42 +0200

    rgetdd

--- P1/src/paqpruebas/Prueba1.java
+++ P1/src/paqpruebas/Prueba1.java
@@ -12,6 +12,7 @@ public class Prueba1 {
 		System.out.println("djsdiujdsiisd");
 		System.out.println("djsdiujdsiisd");
 		System.out.println("djsdiujdsiisd");
+		System.out.println("Otro dato");
 	}
 
 }
