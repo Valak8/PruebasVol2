746458496291569cd8c577096e0ab0015b29d5cd
Galder <Galder@DESKTOP-EU1JRUL>
2020-04-17 18:20:06 +0200

    puto lio

--- P1/src/paqpruebas/Prueba1.java
+++ P1/src/paqpruebas/Prueba1.java
@@ -5,6 +5,7 @@ public class Prueba1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Que dia mas maravilloso");
+		System.out.println("No esto es un puto lio");
 	}
 
 }
